f2258c2cca5153101963b4d8ee44c557650dd631
Mark Wallace <mark@leker.to>
2024-12-09 15:59:46 -0800

    Added boilerplate for next.js React client

---
+++ chat-client/src/app/page.tsx
@@ -0,0 +1,11 @@
+import MyComponent from "@/components/MyComponent";
+
+// app/page.tsx
+export default function Page() {
+  return (
+    <div>
+      {/* Your page content here */}
+      <MyComponent />
+    </div>
+  );
+}
---
+++ chat-client/src/components/MyComponent.tsx
@@ -0,0 +1,12 @@
+// src/components/MyComponent.tsx
+import React from 'react';
+
+interface MyComponentProps {
+  // Add prop types here
+}
+
+const MyComponent: React.FC<MyComponentProps> = () => {
+  return <div>Hello, world!</div>;
+};
+
+export default MyComponent;
